9eae203ac699f5bfc38ef9e23f30ffcb8ea79a53
Shadow <darth3157@gmail.com>
2025-04-14 23:06:35 +0500

    First Commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This includes all files inside /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ server/controllers/userController.js
@@ -0,0 +1,7 @@
+
+
+// Api COntroller Function to Manage Clerk User with Database
+// http://localhost:4000/api/user/webhooks
+const clerkWebhooks = async (req,res) => {
+    
+}
